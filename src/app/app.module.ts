9f4ab9c0dfb8af36ed887cfbd6733c7119232544
Sergio Catalano <shcatalano53@gmail.com>
2021-12-06 15:14:49 -0300

    cambios agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    PokemonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
